86b0514965e1654f2b13d3a95bb3aa6898aab51d
Arulraj S <119486304+SArulRaj24@users.noreply.github.com>
2025-07-30 17:22:52 +0530

    Update Topcontent.jsx

--- src/Components/Topcontent/Topcontent.jsx
+++ src/Components/Topcontent/Topcontent.jsx
@@ -12,7 +12,7 @@ const Topcontent = () => {
             <h1 className="topcontent__name">I'M, S Arulraj</h1>
             <p className="topcontent__desc">I'm an inventive software engineer with a strong focus on AI, driven by curiosity and a clear sense of purpose. I enjoy solving complex problems and turning them into real-world solutions through code. I'm always eager to learn, adapt, and make a meaningful impact at scale.</p>
             <div className="topcontent__btns">
-              <a href="https://drive.google.com/file/d/15aOO6_beQclW-YgFr_3leHpUPGav4kaN/view?usp=drivesdk" target="_blank" rel="noopener noreferrer">
+              <a href="https://drive.google.com/file/d/1sqD4cAMHbNDMPR0GJcQwxtiUjJgR9p7Y/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                 <button className='topcontent__cvbtn'>Resume</button>
               </a>
               <Link to='project' smooth={true} duration={500} offset={-100}>
